23edfcd674d97ab79f6cf8c957b09f57322af3f8
jdavi241 <davisjimm25@gmail.com>
2022-05-08 16:20:37 -0500

    password conditions

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var correctPrompts = getPrompts (); 
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -19,3 +21,31 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Condition prompts 
+function getPrompts() {
+  choiceArray = [];
+
+  characterLength = parseInt(prompt("Enter desired password length 8-128 characters."));
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length must be 8-128 characters.");
+    return false;
+  }
+
+  if (confirm("Would you like lower case letters? Click 'Okay' for yes and 'Cancel' for no.")) {
+    choiceArray = choiceArray.concat(lowerCaseArray);
+  }
+
+  if (confirm("Would you like upper case letters? Click 'Okay' for yes and 'Cancel' for no.")) {
+    choiceArray = choiceArray.concat(upperCaseArray);
+  }
+
+  if (confirm("Would you like to include numbers? Click 'Okay' for yes and 'Cancel' for no.")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+
+  if (confirm("Would you like to include special characters? Click 'Okay' for yes and 'Cancel' for no.")) {
+    choiceArray = choiceArray.concat(specialCharacterArray);
+  }
+  return true;
+}
